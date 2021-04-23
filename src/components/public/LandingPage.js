7860c96a9ef39afd356bac6295008d91567d4b8d
Lorenzo amodeo <lrnzmd@gmail.com>
2021-04-23 13:15:50 +0200

    fix landing image

--- src/components/public/LandingPage.js
+++ src/components/public/LandingPage.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import '../../styles/LandingPage.css'
 
 export default function LandingPage() {
     return (
-        <Container>
-            <Row>
-                <Col md={6}>
+        <Container className="landing-container">
+            <Row className="landing-row">
+                <Col  md={6} sm={12} className="landing-col">
                     ciao
                 </Col>
-                <Col  md={6}>
-                    elena
+                <Col  md={6} sm={12} className="landing-col">
+                    <div className="sfondolanding">
+                        bella
+                    </div>
                 </Col>
             </Row>
         </Container>
