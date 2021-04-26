93df78240ec7c7ed30740d0ebb5ee1a41015f43f
Lorenzo amodeo <lrnzmd@gmail.com>
2021-04-26 09:50:14 +0200

    form register popup

--- src/components/public/LandingPage.js
+++ src/components/public/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import "../../styles/LandingPage.css";
+import LoginForm from "./LoginForm";
 
 export default function LandingPage() {
   return (
@@ -10,26 +11,7 @@ export default function LandingPage() {
           <Row>
             <Col className="d-flex justify-content-center ">
               <div className="form-div">
-                <Form>
-                  <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                      We'll never share your email with anyone else.
-                    </Form.Text>
-                  </Form.Group>
-
-                  <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                  </Form.Group>
-                  <Button variant="primary" type="submit">
-                    Submit
-                  </Button>
-                </Form>
+                <LoginForm/>
               </div>
             </Col>
           </Row>
--- src/components/public/LoginForm.js
+++ src/components/public/LoginForm.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState} from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import RegisterForm from "./RegisterForm";
 
 export default function LoginForm() {
-    return (
-        <div>
-            
+
+  const [modalShow, setModalShow] = useState(false);
+
+  const registerPres = (e) =>{
+    e.preventDefault()
+    setModalShow(true)
+
+  }
+
+  return (
+    <div>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <div className="btn-container">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+
+          <Button variant="primary" type="submit" onClick={registerPres}>
+            Register
+          </Button>
+          <RegisterForm show={modalShow} onHide={() => setModalShow(false)} />
         </div>
-    )
+      </Form>
+    </div>
+  );
 }
