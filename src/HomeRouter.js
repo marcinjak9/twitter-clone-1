142f07002c17a16ef8cb8925e4bf6cfdf31fb409
Lorenzo amodeo <lrnzmd@gmail.com>
2021-04-30 12:42:09 +0200

    token salvataggio

--- src/HomeRouter.js
+++ src/HomeRouter.js
@@ -1,20 +1,42 @@
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./components/private/Home";
 import LandingPage from "./components/public/LandingPage";
 
 export default function HomeRouter() {
-
   const [token, setToken] = useState("");
+
+  useEffect(() => {
+    if(token){
+      salvaToken(token);
+    } 
+  }, [token]);
+
+  useEffect(() => {
+    chiamaToken();
+  }, []);
+
+  const salvaToken = (token) => {
+    localStorage.setItem("token", `${token}`);
+  };
+
+  const chiamaToken = () => {
+    const itemToken = localStorage.getItem("token");
+    if (itemToken !== "") {
+      setToken(itemToken);
+    } else {
+      console.log("token non trovato");
+    }
+  };
 
   return (
     <Router>
       <Switch>
-        <Route path="/home" >
-          <Home token={token}/>
+        <Route path="/home">
+          <Home token={token} />
         </Route>
         <Route path="/">
-          <LandingPage token={token} setToken={setToken}/>
+          <LandingPage token={token} setToken={setToken} />
         </Route>
       </Switch>
     </Router>
--- src/components/private/TweetForm.js
+++ src/components/private/TweetForm.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import { Form, Button, Card } from "react-bootstrap";
 import "../../styles/HomePage.css"
 
-export default function TweetForm() {
+export default function TweetForm({token}) {
+
+  const [tweets, setTweets] = useState([]);
+  const [text, setText] = useState("");
+  
+
+  const newTweet = () =>{
+    fetch("https://secret-temple-42258.herokuapp.com/tweets/", {
+      method: "POST",
+      body: {text : text},
+      headers: { "Content-Type": "application/json", "Authorization" : `Bearer ${token}`},
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json) );
+  }
+
+
   return (
     <div>
     <Card>
       <Form className="form-tweet">
         <Form.Group className="form-text">
-          <Form.Control type="email" placeholder="scrivi un tweet" />
+          <Form.Control type="text" placeholder="scrivi un tweet"  onChange={(e) => setText(e.target.value)}/>
         </Form.Group>
         <div className="form-footer-btn">
-        <Button className="btn-tweet">tweet</Button>
+        <Button className="btn-tweet" onClick={newTweet}>tweet</Button>
         </div>
       </Form>
       </Card>
