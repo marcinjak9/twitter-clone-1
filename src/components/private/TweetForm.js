d31f150e4e0c9fe53b48c72f19be6697ca32f498
lrnzmd <lrnzmd@gmail.com>
2021-05-14 10:56:57 +0200

    fix clear form

--- src/components/private/TweetForm.js
+++ src/components/private/TweetForm.js
@@ -1,32 +1,46 @@
-import React,{useState} from "react";
+import React, { useRef, useState } from "react";
 import { Form, Button, Card } from "react-bootstrap";
-import "../../styles/HomePage.css"
+import "../../styles/HomePage.css";
 import ApiUtils from "./ApiUtils";
 
-export default function TweetForm({fetchTweets}) {
+export default function TweetForm({ fetchTweets }) {
   const [text, setText] = useState("");
-  
-  const newTweet = (e) =>{
-  e.preventDefault();
-  ApiUtils("tweets","POST", {text} )
-      .then((json) => {
+
+   const tweetInput  = useRef(null)
+
+  const newTweet = (e) => {
+    e.preventDefault();
+    if (e.target.value !== "" && text !== "") {
+      ApiUtils("tweets", "POST", { text }).then((json) => {
         fetchTweets();
         setText("");
+        tweetInput.current.value = "";
       });
-  }
+    } else {
+      return alert("scrivi il cazzo di tweet");
+    }
+  };
 
 
   return (
     <div>
-    <Card>
-      <Form className="form-tweet" onSubmit={newTweet}>
-        <Form.Group className="form-text">
-          <Form.Control type="text" placeholder="scrivi un tweet" onChange={(e) => setText(e.target.value)}/>
-        </Form.Group>
-        <div className="form-footer-btn">
-        <Button className="btn-tweet" type="submit" >tweet</Button>
-        </div>
-      </Form>
+      <Card>
+        <Form className="form-tweet" onSubmit={newTweet}>
+          <Form.Group className="form-text">
+            <Form.Control
+              ref={tweetInput}
+              type="text"
+              placeholder="scrivi un tweet"
+              required="required"
+              onChange={(e) => setText(e.target.value)}
+            />
+          </Form.Group>
+          <div className="form-footer-btn">
+            <Button className="btn-tweet" type="submit">
+              tweet
+            </Button>
+          </div>
+        </Form>
       </Card>
     </div>
   );
