75b209c775b447713998d7a651bdabddb7666d2d
lrnzmd <lrnzmd@gmail.com>
2021-05-21 12:10:49 +0200

    fix heart icon load

--- src/components/private/TweetCard.js
+++ src/components/private/TweetCard.js
@@ -8,6 +8,7 @@ import {
   faUpload,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartoutline } from "@fortawesome/free-regular-svg-icons";
 import ApiUtils from "./ApiUtils";
 import { Button } from "react-bootstrap";
 
@@ -15,7 +16,8 @@ export default function TweetCard({ tweetsitem, key }) {
   const { _id, text, created, updated, tags, creator } = tweetsitem;
   const userId = localStorage.getItem("userId");
   const [like, setLike] = useState(0);
-  const [liked, setLiked] = useState();
+  const [liked, setLiked] = useState([]);
+  const [loadingAlreadyLiked, setLoadingAlreadyLiked] = useState(null);
 
   // Con questo useffect settiamo lo stato iniziale del nostro tweet, e rispondiamo ad eventuali cambiamenti
   useEffect(() => {
@@ -23,18 +25,21 @@ export default function TweetCard({ tweetsitem, key }) {
     setLiked(tweetsitem.liked);
   }, [tweetsitem]);
 
+  // con find troviamo se nella lista utenti dentro tweetsitem.liked c'è il nostro user ID
+  const alreadyLiked = liked.find((users) => {
+    return users.id === userId;
+  });
+
   // BUG: se si clicca velocemente partono diverse chiamate e finisce in errore
   // Una soluzione potrebbe essere disabilitare con uno stato il pulsante mentre la chiamata è in carso (in caso mettere un icona di caricamento)
   const triggerLike = () => {
-    // con find troviamo se nella lista utenti dentro tweetsitem.liked c'è il nostro user ID
-    const alreadyLiked = liked.find((users) => {
-      return users.id === userId;
-    });
     // Se esiste allora procediamo con unlike, senno con un like
     if (alreadyLiked) {
       unlikeTweet();
+      setLoadingAlreadyLiked(false);
     } else {
       likeTweet();
+      setLoadingAlreadyLiked(true);
     }
   };
 
@@ -42,6 +47,7 @@ export default function TweetCard({ tweetsitem, key }) {
     ApiUtils(`tweets/like/${_id}`, "POST").then((json) => {
       setLike(json.like);
       setLiked(json.liked);
+      setLoadingAlreadyLiked(null);
     });
   };
 
@@ -49,8 +55,24 @@ export default function TweetCard({ tweetsitem, key }) {
     ApiUtils(`tweets/unlike/${_id}`, "POST").then((json) => {
       setLike(json.like);
       setLiked(json.liked);
+      setLoadingAlreadyLiked(null);
     });
   };
+
+  const renderIcon = () =>{
+      if(loadingAlreadyLiked !== null){
+        if(loadingAlreadyLiked){
+          return faHeart
+        }
+        return faHeartoutline
+      };
+      
+      if (alreadyLiked){
+        return faHeart
+      }
+      return faHeartoutline;
+  
+  }
 
   return (
     <div className="mb-2 mt-2">
@@ -72,8 +94,16 @@ export default function TweetCard({ tweetsitem, key }) {
 
           <div className="tweet-icon-container">
             {/* Non è mai una buona pratica usare onClick su un elemento diverso da button, in questo caso ho racchiuso le icon dentro i bnutton di Bootstrap */}
-            <Button size="sm" onClick={triggerLike}>
-              <FontAwesomeIcon size="xs" icon={faHeart} /> {like}
+            <Button
+              size="sm"
+              onClick={triggerLike}
+              disabled={loadingAlreadyLiked !== null}
+            >
+              <FontAwesomeIcon
+                size="xs"
+                icon={renderIcon()}
+              />{" "}
+              {like}
             </Button>
             <Button size="sm" onClick={triggerLike}>
               <FontAwesomeIcon size="xs" icon={faComment} />
